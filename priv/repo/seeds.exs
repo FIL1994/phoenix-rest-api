6fd3dd1d0569329e1d386bd6d6b7e7f667fb1728
Philip Van Raalte <phil.vanraalte@gmail.com>
2019-04-08 19:59:48 -0400

    added user to seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,5 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+MyApp.Auth.create_user(%{email: "asd@asd.com", password: "qwerty"})
